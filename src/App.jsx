e5e3809f885a4b2745faff2862a22da74e925165
chad hinds <chadrickj8@gmail.com>
2023-06-26 10:42:53 -0400

    Add increament and decreament counter functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+function increase() {
+  setCount( count + 1)
+}
 
+function decrease() {
+  if(count <= 0) {
+    setCount(0)
+  }
+  else {
+    setCount( count -1 )
+  }
+}
+  console.log(count)
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button className='text-xl' onClick={()=> increase()}>Increase</button>
+        <button className='text-xl' onClick={()=> decrease()}>Decrease</button>
+        <p className='text-3xl font-bold underline'>Currrent count: {count}</p>
+
     </>
   )
 }
