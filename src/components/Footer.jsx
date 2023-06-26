367eeb4d58f17e4d42b1b48167cfa17ba69d10d1
chad hinds <chadrickj8@gmail.com>
2023-06-26 10:53:00 -0400

    Include social links in footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <a className="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase" href="https://github.com/chadhindsight">Github</a>
+            <a className="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase" href="https://www.linkedin.com/in/chadrick-hinds">LinkedIn</a>
+            <a className="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase" href="https://twitter.com/Chadhindsight">Twitter</a>
+        </footer>
+    );
+};
+
+export default Footer;
